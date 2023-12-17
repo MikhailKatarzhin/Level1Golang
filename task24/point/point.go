50a2cd2aa4c73eef0dc2478856eb24ccc03d4064
Katarzhin Mikhail <maikl.1997@mail.ru>
2023-12-18 03:38:01 +0400

    [feat] Task24

--- task24/point/point.go
+++ task24/point/point.go
@@ -7,12 +7,12 @@ type Point struct {
 	x, y float64
 }
 
-// Конструктор для создания новой точки
+// NewPoint Конструктор для создания новой точки
 func NewPoint(x, y float64) Point {
 	return Point{x, y}
 }
 
-// Метод для вычисления расстояния между двумя точками
+// DistanceTo вычисляет расстояние между двумя точками
 func (p Point) DistanceTo(other Point) float64 {
 	dx := other.x - p.x
 	dy := other.y - p.y
